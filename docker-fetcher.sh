1dea80ea6ce9b2fb883a85965e4af4cae52a13ad
ikebo <k_1043@126.com>
2021-03-05 14:16:44 +0800

    add docker fetcher

---
+++ docker-fetcher.sh
@@ -0,0 +1,16 @@
+# a
+#apt-get install docker.io
+#(universe sources) https://packages.ubuntu.com/xenial-updates/docker.io
+
+#b
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+apt-get update
+
+apt-get install -y docker-ce
+
+docker run hello-world
+
